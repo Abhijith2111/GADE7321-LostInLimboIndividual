6835c298f6a33e10d59b33c05e91684e831caaaa
DavidBarker05 <davidbarker9611@gmail.com>
2026-02-13 18:39:50 +0200

    added comments and proper custom exceptions

--- Assets/OutAssets/Scripts/QueueADT.cs
+++ Assets/OutAssets/Scripts/QueueADT.cs
@@ -1,26 +1,65 @@
+/// <summary>
+/// A "first in first out" data structure
+/// </summary>
+/// <typeparam name="T">The type of data stored in the queue</typeparam>
 public class QueueADT<T>
 {
     System.Collections.Generic.List<T> fifo = new System.Collections.Generic.List<T>();
 
+    /// <summary>
+    /// The number of items in the queue
+    /// </summary>
     public int Count => fifo.Count;
 
+    /// <summary>
+    /// Indicates whether or not the queue has items or not
+    /// </summary>
     public bool IsEmpty => Count == 0;
     
+    /// <summary>
+    /// Add an item to the back of the queue
+    /// </summary>
+    /// <param name="item">The item to add to the queue</param>
     public void Enqueue(T item) => fifo.Add(item);
 
+    /// <summary>
+    /// Get the item at the front of the queue and remove it from the queue
+    /// </summary>
+    /// <returns>The item at the front of the queue</returns>
+    /// <exception cref="EmptyQueueException">Thrown if the queue has no items</exception>
     public T Dequeue()
     {
-        if (IsEmpty) throw new System.Exception("EmptyQueueException");
+        if (IsEmpty) throw new EmptyQueueException("The queue has no items to access");
         T item = fifo[0];
         fifo.RemoveAt(0);
         return item;
     }
 
+    /// <summary>
+    /// Get the item at the front of the queue
+    /// </summary>
+    /// <returns>The item at the front of the queue</returns>
+    /// <exception cref="EmptyQueueException">Thrown if the queue has no items</exception>
     public T Peek()
     {
-        if (IsEmpty) throw new System.Exception("EmptyQueueException");
+        if (IsEmpty) throw new EmptyQueueException("The queue has no items to access");
         return fifo[0];
     }
 
+    /// <summary>
+    /// Clears all items in the queue
+    /// </summary>
     public void Clear() => fifo.Clear();
+}
+
+/// <summary>
+/// Exception thrown when trying to access or remove an item from an empty queue
+/// </summary>
+public class EmptyQueueException : System.Exception
+{
+    public EmptyQueueException() { }
+
+    public EmptyQueueException(string message) : base(message) { }
+
+    public EmptyQueueException(string message, System.Exception inner) : base(message, inner) { }
 }
--- Assets/OutAssets/Scripts/StackADT.cs
+++ Assets/OutAssets/Scripts/StackADT.cs
@@ -1,26 +1,65 @@
+/// <summary>
+/// A "last in first out" data structure
+/// </summary>
+/// <typeparam name="T">The type of data stored in the stack</typeparam>
 public class StackADT<T>
 {
     System.Collections.Generic.List<T> lifo = new System.Collections.Generic.List<T>();
 
+    /// <summary>
+    /// The number of items in the stack
+    /// </summary>
     public int Count => lifo.Count;
 
+    /// <summary>
+    /// Indicates whether or not the stack has items or not
+    /// </summary>
     public bool IsEmpty => Count == 0;
 
+    /// <summary>
+    /// Add an item to the back of the stack
+    /// </summary>
+    /// <param name="item">The item to add to the stack</param>
     public void Push(T item) => lifo.Add(item);
 
+    /// <summary>
+    /// Get the item at the back of the stack and remove it from the stack
+    /// </summary>
+    /// <returns>The item at the back of the stack</returns>
+    /// <exception cref="EmptyStackException">Thrown if the stack has no items</exception>
     public T Pop()
     {
-        if (IsEmpty) throw new System.Exception("EmptyStackException");
+        if (IsEmpty) throw new EmptyStackException("The stack has no items to access");
         T item = lifo[lifo.Count - 1];
         lifo.RemoveAt(lifo.Count - 1);
         return item;
     }
 
+    /// <summary>
+    /// Get the item at the back of the stack
+    /// </summary>
+    /// <returns>The item at the back of the stack</returns>
+    /// <exception cref="EmptyStackException">Thrown if the stack has no items</exception>
     public T Peek()
     {
-        if (IsEmpty) throw new System.Exception("EmptyStackException");
+        if (IsEmpty) throw new EmptyStackException("The stack has no items to access");
         return lifo[lifo.Count - 1];
     }
 
+    /// <summary>
+    /// Clears all items in the stack
+    /// </summary>
     public void Clear() => lifo.Clear();
+}
+
+/// <summary>
+/// Exception thrown when trying to access or remove an item from an empty stack
+/// </summary>
+public class EmptyStackException : System.Exception
+{
+    public EmptyStackException() { }
+
+    public EmptyStackException(string message) : base(message) { }
+
+    public EmptyStackException(string message, System.Exception inner) : base(message, inner) { }
 }
