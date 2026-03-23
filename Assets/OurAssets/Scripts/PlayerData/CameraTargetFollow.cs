979ec7327c4246858899cc63b71de326c6acf9cf
DavidBarker05 <davidbarker9611@gmail.com>
2026-03-23 13:09:39 +0200

    Changed offset and made it use late update
    use late update because then the target won't update its position before the player moves which created a jittery effect

--- Assets/OurAssets/Scripts/PlayerData/CameraTargetFollow.cs
+++ Assets/OurAssets/Scripts/PlayerData/CameraTargetFollow.cs
@@ -5,7 +5,7 @@ public class CameraTargetFollow : MonoBehaviour
     [SerializeField]
     Transform player;
     [SerializeField]
-    Vector3 offset = new Vector3(0f, 0.7f, 0f);
+    Vector3 offset = new Vector3(0f, 1.375f, 0f);
 
-    void Update() => transform.position = player.position + offset;
+    void LateUpdate() => transform.position = player.position + offset;
 }
