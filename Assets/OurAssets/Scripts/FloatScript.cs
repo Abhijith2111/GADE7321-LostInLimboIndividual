0fb6f217e59c7f0694d1bec6b35949dd391ab501
DavidBarker05 <davidbarker9611@gmail.com>
2026-03-30 20:35:21 +0200

    added ability to reduce bobbing

--- Assets/OurAssets/Scripts/FloatScript.cs
+++ Assets/OurAssets/Scripts/FloatScript.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class FloatScript : MonoBehaviour
 {
+    [SerializeField, Range(0f, 1f)]
+    float bobbingScaleFactor = 1f;
+
     Vector3 startingPosition;
 
     void Start() => startingPosition = transform.position;
 
-    void LateUpdate() => transform.position = startingPosition + Vector3.up * WaterWaveManager.Instance.GetWaterHeight(startingPosition);
+    void LateUpdate() => transform.position = startingPosition + (WaterWaveManager.Instance.GetWaterHeight(startingPosition) * bobbingScaleFactor) * Vector3.up;
 }
